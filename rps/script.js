21c63e04ba0891671ba6f0eb48db9fe0bdc9931b
Jerry Ho Nai Yi <jerry.ho@qinetics.local>
2019-03-04 12:12:20 +0800

    3rd commit

--- rps/script.js
+++ rps/script.js
@@ -4,109 +4,127 @@ var loseCounter = 0
 var winCounter = 0
 var drawCounter = 0
 
-function startGuess(guess){
-  var countdown3 = document.getElementById("countdown3")
-  var countdown2 = document.getElementById("countdown2")
-  var countdown1 = document.getElementById("countdown1")
-  var result = document.getElementById("result")
+var countdown3 = document.getElementById("countdown3")
+var countdown2 = document.getElementById("countdown2")
+var countdown1 = document.getElementById("countdown1")
+var result = document.getElementById("result")
 
+var drawElement = document.getElementById("drawCount")
+var winElement = document.getElementById("winCount")
+var loseElement = document.getElementById("loseCount")
+
+var resultPaper = document.getElementById("result-img-paper")
+var resultRock = document.getElementById("result-img-rock")
+var resultScissors = document.getElementById("result-img-scissors")
+
+
+
+function startGuess(guess) {
+  resultPaper.style.display = "none"
+  resultRock.style.display = "none"
+  resultScissors.style.display = "none"
+  
   result.style.display = "none"
   countdown3.style.display = "block"
-  setTimeout(()=>{
+  setTimeout(() => {
     countdown3.style.display = "none"
     countdown2.style.display = "block"
-    setTimeout(()=>{
+    setTimeout(() => {
       countdown2.style.display = "none"
       countdown1.style.display = "block"
-        setTimeout(()=>{
-          countdown1.style.display = "none"
-          userGuess(guess)
-        },1000)
-      },1000)
-    },1000)
+      setTimeout(() => {
+        countdown1.style.display = "none"
+        userGuess(guess)
+      }, 1000)
+    }, 1000)
+  }, 1000)
 }
 
-function userGuess(guess){
-  var result = document.getElementById("result")
+function userGuess(guess) {
 
-  var drawElement = document.getElementById("drawCount")
-  var winElement = document.getElementById("winCount")
-  var loseElement = document.getElementById("loseCount")
-  
-  if(guess == "rock" && computerChoice == "rock"){
+  if (guess == "rock" && computerChoice == "rock") {
     drawCounter++
     drawElement.innerHTML = drawCounter
     result.innerHTML = "DRAW"
     result.style.display = "block"
     result.style.color = "black"
+    document.getElementById("result-img-" + computerChoice).style.display = "block"
 
   }
-  else if(guess == "rock" && computerChoice == "paper"){
+  else if (guess == "rock" && computerChoice == "paper") {
     loseCounter++
     loseElement.innerHTML = loseCounter
     result.innerHTML = "LOSE"
     result.style.display = "block"
     result.style.color = "red"
+    document.getElementById("result-img-" + computerChoice).style.display = "block"
   }
-  else if(guess == "rock" && computerChoice == "scissors"){
+  else if (guess == "rock" && computerChoice == "scissors") {
     winCounter++
     winElement.innerHTML = winCounter
     result.innerHTML = "WIN"
     result.style.display = "block"
     result.style.color = "green"
+    document.getElementById("result-img-" + computerChoice).style.display = "block"
   }
-  else if(guess == "paper" && computerChoice == "rock"){
+  else if (guess == "paper" && computerChoice == "rock") {
     winCounter++
     loseElement.innerHTML = winCounter
     result.innerHTML = "WIN"
     result.style.display = "block"
     result.style.color = "green"
+    document.getElementById("result-img-" + computerChoice).style.display = "block"
   }
-  else if(guess == "paper" && computerChoice == "paper"){
+  else if (guess == "paper" && computerChoice == "paper") {
     drawCounter++
     drawElement.innerHTML = drawCounter
     result.innerHTML = "DRAW"
     result.style.display = "block"
     result.style.color = "black"
+    document.getElementById("result-img-" + computerChoice).style.display = "block"
   }
-  else if(guess == "paper" && computerChoice == "scissors"){
+  else if (guess == "paper" && computerChoice == "scissors") {
     loseCounter++
     drawElement.innerHTML = loseCounter
     result.innerHTML = "LOSE"
     result.style.display = "block"
     result.style.color = "red"
+    document.getElementById("result-img-" + computerChoice).style.display = "block"
   }
-   else if(guess == "scissors" && computerChoice == "rock"){
+  else if (guess == "scissors" && computerChoice == "rock") {
     loseCounter++
     loseElement.innerHTML = loseCounter
     result.innerHTML = "LOSE"
     result.style.display = "block"
     result.style.color = "red"
+    document.getElementById("result-img-" + computerChoice).style.display = "block"
   }
-  else if(guess == "scissors" && computerChoice == "paper"){
+  else if (guess == "scissors" && computerChoice == "paper") {
     winCounter++
     winElement.innerHTML = winCounter
     result.innerHTML = "WIN"
     result.style.display = "block"
     result.style.color = "green"
+    document.getElementById("result-img-" + computerChoice).style.display = "block"
   }
-  else if(guess == "scissors" && computerChoice == "scissors"){
+  else if (guess == "scissors" && computerChoice == "scissors") {
     drawCounter++
     drawElement.innerHTML = drawCounter
     result.innerHTML = "DRAW"
     result.style.display = "block"
     result.style.color = "black"
+    document.getElementById("result-img-" + computerChoice).style.display = "block"
   }
-    
+
   computerChoose()
 }
 
 //part 2
-var options = ["paper","scissors","rock"]
+var options = ["paper", "scissors", "rock"]
 var computerChoice
 
-function computerChoose(){
-  var randomNumber = Math.floor(Math.random() * options.length )
+function computerChoose() {
+  var randomNumber = Math.floor(Math.random() * options.length)
   computerChoice = options[randomNumber]
   //alert(computerChoice)
 }
